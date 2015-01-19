d3cc7171e6c62133994311d4944aa6bf52707c92
gorjanz <gorjanz@gmail.com>
2015-01-19 18:50:02 +0100

    added additional file name constants

--- Events-rs-poc/src/rs/poc/utils/Constants.java
+++ Events-rs-poc/src/rs/poc/utils/Constants.java
@@ -2,10 +2,50 @@ package rs.poc.utils;
 
 public final class Constants {
 
-	public static String JOINED_AT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-	public static String FILES_SOURCE_DIRECTORY = "D:\\Fax\\Diplomska\\kaggle\\";
-	public static String USERS_DATA = FILES_SOURCE_DIRECTORY + "users.csv";
-	public static String EVENTS_DATA = FILES_SOURCE_DIRECTORY + "events0.csv";
-	public static String USER_FRIENDS_DATA = FILES_SOURCE_DIRECTORY + "user_friends.csv";
+	public static final String EVENT_START_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+	public static final String INVITED_AT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	
+	public static final String FILES_SOURCE_DIRECTORY = "D:\\Fax\\Diplomska\\kaggle\\";
+	public static final String CLEAN_FILES_SOURCE_DIRECTORY = FILES_SOURCE_DIRECTORY + "clean-data\\";
+	public static final String USERS_DATA = FILES_SOURCE_DIRECTORY + "users.csv";
+	public static final String EVENTS_DATA = FILES_SOURCE_DIRECTORY + "events.csv";
+	public static final String EVENTS_DATA_0 = FILES_SOURCE_DIRECTORY + "events0.csv";
+	public static final String EVENTS_DATA_1 = FILES_SOURCE_DIRECTORY + "events1.csv";
+	public static final String EVENTS_DATA_2 = FILES_SOURCE_DIRECTORY + "events2.csv";
+	public static final String EVENTS_DATA_3 = FILES_SOURCE_DIRECTORY + "events3.csv";
+	public static final String EVENTS_DATA_4 = FILES_SOURCE_DIRECTORY + "events4.csv";
+	public static final String EVENTS_DATA_5 = FILES_SOURCE_DIRECTORY + "events5.csv";
+	public static final String EVENTS_DATA_6 = FILES_SOURCE_DIRECTORY + "events6.csv";
+	public static final String EVENTS_DATA_7 = FILES_SOURCE_DIRECTORY + "events7.csv";
+	public static final String EVENTS_DATA_8 = FILES_SOURCE_DIRECTORY + "events8.csv";
+	public static final String EVENTS_DATA_9 = FILES_SOURCE_DIRECTORY + "events9.csv";
+	public static final String EVENTS_DATA_10 = FILES_SOURCE_DIRECTORY + "events10.csv";
+	public static final String USER_FRIENDS_DATA = FILES_SOURCE_DIRECTORY + "user_friends.csv";
+	public static final String TEST_DATA = FILES_SOURCE_DIRECTORY + "test.csv";
+	public static final String EVENT_ATTENDEES_DATA = FILES_SOURCE_DIRECTORY + "event_attendees.csv";
+	
+	public static final String CLEAN_USERS_DATA = CLEAN_FILES_SOURCE_DIRECTORY + "cleanUsers.csv";
+	public static final String CLEAN_EVENTS_DATA = CLEAN_FILES_SOURCE_DIRECTORY + "cleanEvents.csv";
+	public static final String CLEAN_FRIENDS_DATA = CLEAN_FILES_SOURCE_DIRECTORY + "cleanFriends.csv";
+	public static final String CLEAN_ATTENDEES_DATA = CLEAN_FILES_SOURCE_DIRECTORY + "cleanAttendees.csv";
+	
+	public static final String TRAIN_FILES_SOURCE_DIRECTORY = FILES_SOURCE_DIRECTORY + "train-data\\";
+	public static final String TRAIN_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "train.csv";
+	public static final String TRAIN_USERS_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "usersTrain.csv";
+	public static final String TRAIN_EVENTS_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "eventsTrain.csv";
+	public static final String TRAIN_FRIENDS_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "friendsTrain.csv";
+	public static final String TRAIN_ATTENDEES_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "attendeesTrain.csv";
+	
+	public static final String TRAIN_ARFF_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "train.arff";
+	public static final String TEST_ARFF_DATA = TRAIN_FILES_SOURCE_DIRECTORY + "test.arff";
+	
+	public static final String USER_TRAINING_FILES_DIRECTORY = FILES_SOURCE_DIRECTORY + "user-training-files\\";
+	public static final String USER_TEST_FILES_DIRECTORY = FILES_SOURCE_DIRECTORY + "user-test-files\\";
+	
+	public static final String BENCHMARK_DATA = FILES_SOURCE_DIRECTORY + "event_popularity_benchmark.csv";
+	
+	
+	public static final Double EVENT_RADIUS = 100.00;
+	public static final String RESULT_DATA = FILES_SOURCE_DIRECTORY + "initialResults.txt";;
 	
 }
